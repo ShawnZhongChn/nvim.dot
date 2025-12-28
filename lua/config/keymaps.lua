d9312e37ed426ed07498051a131d572fa6c3be3e
ShawnZhong <shawnzhongchn@gmail.com>
2025-12-28 23:01:04 +0800

    add jk to Esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 插入模式
+vim.keymap.set("i", "jk", "<C-\\><C-n>", { noremap = true })
+-- 终端模式
+vim.keymap.set("t", "jk", [[<C-\><C-n>]], { noremap = true })
+-- 可选：连键时间
+vim.o.timeoutlen = 300
